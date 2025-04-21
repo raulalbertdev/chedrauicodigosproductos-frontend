3f45d1c508cba1635a02cf5b40b99ea4130411b6
raulalbertdev <raulalbertdev@gmail.com>
2025-04-21 12:01:47 -0500

    Añadir registro de actividad por cada buscador de departamento en especifico

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const loadApp = async ()  => {
+const loadApp = async (statusActivity)  => {
     await fetch("https://api.chedrauicodigosean.raulalbert.dev/registerActivityApp", {
       method: "POST",
       headers: {
@@ -6,11 +6,9 @@ const loadApp = async ()  => {
       },
       body: JSON.stringify({
         key: "R4u1*_$Alb3rt||p08!X/;N3t0Che134Ef:C1]}WHgeRKC7HFpQ",
-        status: "loadApp-Main"
+        status: statusActivity
       })
     });
   }
-document.addEventListener("DOMContentLoaded", () => {
-    loadApp ()
-});
+
   
